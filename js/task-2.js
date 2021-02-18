6fdd35ad7cda64f1742973d5e13ded38dd35df0a
Alexander Korol <70759946+Fewess@users.noreply.github.com>
2021-02-18 15:32:51 +0200

    1

---
+++ js/task-2.js
@@ -0,0 +1,21 @@
+const ingredients = [
+  "Картошка",
+  "Грибы",
+  "Чеснок",
+  "Помидоры",
+  "Зелень",
+  "Приправы"
+];
+let ulList = document.getElementById("ingredients");
+const foodIngredients = ingredients.forEach(ingredient=> {
+  let items = document.createElement("li");
+  items.innerHTML = ingredient;
+  ulList.append(items);
+  parent.append(...items)
+});
+
+console.log(foodIngredients);
+
+
+
+
